691f0e9f04d4a5c5acffab80b3df8c49116f8ca2
TahaBo04 <tahab896@gmail.com>
2025-07-08 13:37:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -92,4 +92,5 @@ def cellule_communication():
     return render_template('cellule_communication_externe.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
